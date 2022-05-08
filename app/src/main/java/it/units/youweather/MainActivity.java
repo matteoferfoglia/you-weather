e48ac69fff4c0a9e9f9e85892f74931d780c1b6d
Matteo Ferfoglia <matteoferfoglia@gmail.com>
2022-05-08 17:41:53 +0200

    Add navigation graph and bottom menu

--- app/src/main/java/it/units/youweather/MainActivity.java
+++ app/src/main/java/it/units/youweather/MainActivity.java
@@ -1,14 +1,44 @@
 package it.units.youweather;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import java.util.Objects;
+
+import it.units.youweather.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        // view binding
+        ActivityMainBinding viewBinding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(viewBinding.getRoot());
+
+
+        // nav bar
+
+        NavHostFragment navHostController = (NavHostFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.nav_host_fragment_activity_main);
+        NavController navController = Objects.requireNonNull(navHostController).getNavController();
+
+        BottomNavigationView bottomNavigationMenuView = viewBinding.bottomNavView;
+        NavigationUI.setupWithNavController(bottomNavigationMenuView, navController);
+
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.blankFragment) // these are the ids defined in nav_graph.xml
+                .build();
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+
+        // end nav bar
     }
 }
